0a2c46f954889e4b4ce4eaaa1efcd87c4917e428
HeriYantodotDev <heri@HYA>
2023-02-24 15:44:41 +0700

    server rendering

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+
 
 const planetRouter = require('./routes/planets/planets.router');
 
@@ -10,6 +12,11 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetRouter);
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
