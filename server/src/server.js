c05f07bdfd9d688ebce2aa26db38b50d5483c90d
HeriYantodotDev <heri@HYA>
2023-02-26 12:40:57 +0700

    change load data

--- server/src/server.js
+++ server/src/server.js
@@ -6,11 +6,9 @@ const {loadPlanetsData} = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);
+const server = http.createServer(app); 
 
-async function startServer() {
-    await loadPlanetsData();
-
+function startServer() {
     server.listen(PORT, ()=> {
         console.log(`Listening on PORT ${PORT}`);
     });
